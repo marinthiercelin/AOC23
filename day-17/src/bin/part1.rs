f27ec9dfeaddfbf3b1176d19173458cf11925391
marin thiercelin <marin.thiercelin@protonmail.com>
2024-10-10 09:47:01 +0200

    Day 17 part 1

---
+++ day-17/src/bin/part1.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+use day_17::part1;
+use utils;
+
+fn main() {
+    let input = utils::read_input_file(env::args());
+    let result = part1::run(&input);
+    println!("Result: {result}");
+}
